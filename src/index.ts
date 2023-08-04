26e541e09ba8d0f69d1389bc121612b5b930fe28
aykut <aykut_h42@hotmail.com>
2023-08-05 00:03:42 +0300

    First

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router";
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
